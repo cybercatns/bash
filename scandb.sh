dc3603468437f72f604ead968cabcbbb02f8fd11
Sahan <contactsahan@gmail.com>
2021-05-03 10:50:38 -0700

    sdasd

--- scandb.sh
+++ scandb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# This script will read data from an AWS DynamoDB table and print output to console.
+# This is written by an intern with basic bash and linux skills.  
+
+# You are free to modify this with Linux/BASH/Coding best practices to deploy this to production.
+
 aws --region us-east-1 dynamodb scan --table-name score
 
